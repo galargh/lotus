d06ced5d89e3eb2a784939ce67f3b187c31522da
Piotr Galar <piotr.galar@gmail.com>
2024-07-10 15:45:26 +0100

    Update version.go

--- build/version.go
+++ build/version.go
@@ -39,7 +39,7 @@ func BuildTypeString() string {
 }
 
 // NodeBuildVersion is the local build version of the Lotus daemon
-const NodeBuildVersion string = "1.27.2-dev"
+const NodeBuildVersion string = "1.28.0-rc1"
 
 func NodeUserVersion() BuildVersion {
 	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
